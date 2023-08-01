1eeeb736d152e1266b7b52abc9ea9429227e1982
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-08-01 21:34:46 +0300

    first commit

---
+++ 4.7.1/4.7.1.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <vector>
+
+
+std::map <char, int> m {};
+bool compare(std::pair<char, int>& a, std::pair<char, int>& b)
+{
+    return a.second > b.second;
+}
+
+void fill_map(std::string str) {
+    for (int i = 0; i < str.length(); ++i) {
+        m[str[i]] = 0;
+    }
+    for (int i = 0; i < str.length(); ++i) {
+        m[str[i]] += 1;
+    }
+}
+
+void sort_map(std::vector<std::pair<char, int> > * v) {
+    
+    for (auto& el : m) {
+        (*v).push_back(el);
+    }
+    std::sort((*v).begin(), (*v).end(), compare);
+}
+
+void print_map(std::vector<std::pair<char, int> >* v) {
+    for (auto& el : *v) {
+        std::cout << el.first << " " << el.second << std::endl; 
+    }
+}
+
+int main()
+{
+    std::string str;
+    std::cout << "[IN]: ";
+    //std::getline(std::cin, str); 
+    str = "Hello world!!"; //
+    std::cout << str << std::endl; //
+    fill_map(str);
+    std::vector<std::pair<char, int> > v;
+    sort_map(&v);
+    std::cout << "[OUT]: " << std::endl;
+    print_map(&v);
+    
+}
---
+++ 4.7.2/4.7.2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <array>
+
+int main()
+{
+    std::cout << "[IN]:\n";
+    int size;
+    std::cin >> size;
+    std::set<int, std::greater<int>> set_;
+    int n;
+    for (int i = 0; i < size; ++i) {
+        std::cin >> n;
+        set_.insert(n);
+    }
+    std::cout << "[OUT]:\n";
+    for (auto& it : set_) {
+        std::cout << it << std::endl;
+    }
+}
